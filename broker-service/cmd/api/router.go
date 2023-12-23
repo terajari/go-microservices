c1690b796ad416476408e730ecce81d64f1cf651
agusanggarwanto <agusanggarwanto@gmail.com>
2023-12-23 23:08:17 +0700

    fix: url cors

--- broker-service/cmd/api/router.go
+++ broker-service/cmd/api/router.go
@@ -9,7 +9,7 @@ func (app *Application) routes() *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://*", "https://*"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization", "Accept", "X-CSRF-Token"},
 		ExposeHeaders:    []string{"Link"},
